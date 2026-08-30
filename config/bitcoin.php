42339a4520228184d74ee4e2effdfc075d29af43
faraimupfuti <faraimupfuti@gmail.com>
2026-08-30 20:46:16 +0200

    Set Kosher Market BTC monetization defaults: $200 vendor fee and 3% transaction fee

--- config/bitcoin.php
+++ config/bitcoin.php
@@ -7,4 +7,8 @@ return [
     'btcpay_api_key' => env('BTCPAY_API_KEY'),
     'webhook_secret' => env('BTCPAY_WEBHOOK_SECRET'),
     'required_confirmations' => (int) env('BITCOIN_REQUIRED_CONFIRMATIONS', 1),
+    'vendor_registration_usd' => '200.00',
+    'platform_fee_percent' => env('BITCOIN_PLATFORM_FEE_PERCENT', '3.00'),
+    'admin_btc_address' => env('KOSHER_MARKET_ADMIN_BTC_ADDRESS'),
+    'btc_invoice_currency' => env('BTCPAY_INVOICE_CURRENCY', 'USD'),
 ];
