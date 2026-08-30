bc3aa7d49443e7ba61616a7f727577d7b7ccf3bf
faraimupfuti <faraimupfuti@gmail.com>
2026-08-30 21:59:39 +0200

    Add vendor shipping rate model

---
+++ app/Models/VendorShippingRate.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class VendorShippingRate extends Model
+{
+    protected $fillable = [
+        'shipping_zone_id', 'service_name', 'price_btc', 'free_shipping_threshold_btc',
+        'min_delivery_days', 'max_delivery_days', 'tracking_url_template', 'enabled',
+    ];
+
+    protected $casts = [
+        'price_btc' => 'decimal:8',
+        'free_shipping_threshold_btc' => 'decimal:8',
+        'enabled' => 'boolean',
+    ];
+
+    public function zone() { return $this->belongsTo(VendorShippingZone::class, 'shipping_zone_id'); }
+}
