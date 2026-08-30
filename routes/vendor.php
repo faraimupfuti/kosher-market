4fc1044a2627b90f9b604b0fd29ab7cec570432e
faraimupfuti <faraimupfuti@gmail.com>
2026-08-30 20:11:54 +0200

    Wire vendor Bitcoin payout routes

--- routes/vendor.php
+++ routes/vendor.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Admin\LanguageController;
 use App\Http\Controllers\Vendor\Auth\AuthController;
+use App\Http\Controllers\Vendor\BitcoinPayoutController;
 use App\Http\Controllers\Vendor\DashboardController;
 use App\Http\Controllers\Vendor\OrderController;
 use App\Http\Controllers\Vendor\ProductController;
@@ -22,22 +23,20 @@ Route::prefix('vendor')->group(function () {
         Route::post('products/updateStatus', [ProductController::class, 'updateStatus'])->name('vendor.products.updateStatus');
         Route::resource('social-media-links', SocialMediaLinkController::class)->names('vendor.social-media-links');
         Route::post('social-media-links/data', [SocialMediaLinkController::class, 'getData'])->name('vendor.social-media-links.data');
-
         Route::get('reviews', [ProductReviewController::class, 'index'])->name('vendor.reviews.index');
         Route::get('reviews/data', [ProductReviewController::class, 'getData'])->name('vendor.reviews.data');
         Route::get('reviews/{review}', [ProductReviewController::class, 'show'])->name('vendor.reviews.show');
         Route::delete('reviews/{review}', [ProductReviewController::class, 'destroy'])->name('vendor.reviews.destroy');
-
-        /** Orders */
         Route::get('orders', [OrderController::class, 'index'])->name('vendor.orders.index');
         Route::post('orders/data', [OrderController::class, 'getData'])->name('vendor.orders.data');
         Route::delete('orders/{id}', [OrderController::class, 'destroy'])->name('vendor.orders.destroy');
-
-        /** Language Switch */
         Route::post('/change-language', [LanguageController::class, 'changeLanguage'])->name('vendor.change.language');
-
-        /** Profile */
         Route::get('profile/edit', [ProfileController::class, 'edit'])->name('vendor.profile.edit');
         Route::patch('profile', [ProfileController::class, 'update'])->name('vendor.profile.update');
+
+        // Bitcoin-only seller settlement
+        Route::get('bitcoin/payout', [BitcoinPayoutController::class, 'edit'])->name('vendor.bitcoin.payout');
+        Route::patch('bitcoin/payout', [BitcoinPayoutController::class, 'update'])->name('vendor.bitcoin.payout.update');
+        Route::get('bitcoin/payouts', [BitcoinPayoutController::class, 'index'])->name('vendor.bitcoin.payouts');
     });
 });
