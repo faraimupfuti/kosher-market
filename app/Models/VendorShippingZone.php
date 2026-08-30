238aae38b3934cae6108597e9bc34267214f924b
faraimupfuti <faraimupfuti@gmail.com>
2026-08-30 21:59:35 +0200

    Add vendor shipping zone model

---
+++ app/Models/VendorShippingZone.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class VendorShippingZone extends Model
+{
+    protected $fillable = ['vendor_id', 'name', 'enabled'];
+
+    protected $casts = ['enabled' => 'boolean'];
+
+    public function vendor() { return $this->belongsTo(Vendor::class); }
+    public function countries() { return $this->belongsToMany(Country::class, 'vendor_shipping_zone_countries', 'shipping_zone_id', 'country_id'); }
+    public function rates() { return $this->hasMany(VendorShippingRate::class, 'shipping_zone_id'); }
+}
