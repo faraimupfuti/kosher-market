a277e7f188635baf0326878eb542dc765d790be5
faraimupfuti <faraimupfuti@gmail.com>
2026-09-06 04:44:26 +0200

    feat: notify users of new chat messages

---
+++ app/Notifications/ChatMessageNotification.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Messages\BroadcastMessage;
+use Illuminate\Notifications\Notification;
+
+class ChatMessageNotification extends Notification
+{
+    use Queueable;
+
+    public function __construct(public int $conversationId, public string $senderName, public string $preview)
+    {
+    }
+
+    public function via(object $notifiable): array
+    {
+        return ['database'];
+    }
+
+    public function toArray(object $notifiable): array
+    {
+        return [
+            'type' => 'chat_message',
+            'conversation_id' => $this->conversationId,
+            'sender_name' => $this->senderName,
+            'preview' => $this->preview,
+        ];
+    }
+}
