dda2db62f91dfcade8a39d38c19cdb627504b8df
faraimupfuti <faraimupfuti@gmail.com>
2026-09-06 04:47:58 +0200

    fix: audit admin activity globally

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -14,6 +14,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\SecurityHeaders::class,
+        \App\Http\Middleware\AuditAdminActions::class,
     ];
 
     protected $middlewareGroups = [
