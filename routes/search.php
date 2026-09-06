d1c6d17ca9bfecad47ee366fe59060ad966f8f02
faraimupfuti <faraimupfuti@gmail.com>
2026-09-06 23:43:12 +0200

    feat: expose smart search and saved search routes

--- routes/search.php
+++ routes/search.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\Http\Controllers\Store\SavedSearchController;
+use App\Http\Controllers\Store\SmartSearchController;
 use Illuminate\Support\Facades\Route;
 
+Route::post('/smart-search', [SmartSearchController::class,'interpret'])->name('smart-search');
 Route::middleware('auth:customer')->group(function () {
     Route::get('/saved-searches', [SavedSearchController::class,'index'])->name('saved-searches.index');
     Route::post('/saved-searches', [SavedSearchController::class,'store'])->name('saved-searches.store');
