e21b84cd66b48308c643be0241859c69df7ec89d
faraimupfuti <faraimupfuti@gmail.com>
2026-09-06 23:40:32 +0200

    feat: add vendor analytics route

---
+++ routes/vendor-analytics.php
@@ -0,0 +1,4 @@
+<?php
+use App\Http\Controllers\Vendor\AnalyticsController;
+use Illuminate\Support\Facades\Route;
+Route::middleware('auth:vendor')->prefix('vendor')->group(function () { Route::get('/analytics', [AnalyticsController::class,'index'])->name('vendor.analytics'); });
