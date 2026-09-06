6690cba82d3563e56f9bcf095cdf8c8b1b134329
faraimupfuti <faraimupfuti@gmail.com>
2026-09-06 04:43:43 +0200

    feat: add audit log model

---
+++ app/Models/AuditLog.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AuditLog extends Model
+{
+    protected $fillable = ['actor_type', 'actor_id', 'action', 'route', 'method', 'ip_address', 'user_agent', 'metadata'];
+    protected $casts = ['metadata' => 'array'];
+}
