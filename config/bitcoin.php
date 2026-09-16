6927a7023e24773cad80e018c84dd71218bcc502
faraimupfuti <faraimupfuti@gmail.com>
2026-09-17 00:30:26 +0200

    Align Bitcoin invoice currency configuration

--- config/bitcoin.php
+++ config/bitcoin.php
@@ -10,6 +10,6 @@ return [
     'vendor_registration_usd' => '200.00',
     'platform_fee_percent' => env('BITCOIN_PLATFORM_FEE_PERCENT', '3.00'),
     'admin_btc_address' => env('KOSHER_MARKET_ADMIN_BTC_ADDRESS'),
-    'btc_invoice_currency' => env('BTCPAY_INVOICE_CURRENCY', 'USD'),
+    'btc_invoice_currency' => env('BTCPAY_INVOICE_CURRENCY', 'BTC'),
     'auto_payouts' => filter_var(env('BITCOIN_AUTO_PAYOUTS', true), FILTER_VALIDATE_BOOL),
 ];
