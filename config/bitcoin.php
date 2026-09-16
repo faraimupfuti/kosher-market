d3808655c045c353f0e80185c60175393e38c8ce
faraimupfuti <faraimupfuti@gmail.com>
2026-09-17 00:31:15 +0200

    Require explicit automatic payout opt-in

--- config/bitcoin.php
+++ config/bitcoin.php
@@ -11,5 +11,5 @@ return [
     'platform_fee_percent' => env('BITCOIN_PLATFORM_FEE_PERCENT', '3.00'),
     'admin_btc_address' => env('KOSHER_MARKET_ADMIN_BTC_ADDRESS'),
     'btc_invoice_currency' => env('BTCPAY_INVOICE_CURRENCY', 'BTC'),
-    'auto_payouts' => filter_var(env('BITCOIN_AUTO_PAYOUTS', true), FILTER_VALIDATE_BOOL),
+    'auto_payouts' => filter_var(env('BITCOIN_AUTO_PAYOUTS', false), FILTER_VALIDATE_BOOL),
 ];
